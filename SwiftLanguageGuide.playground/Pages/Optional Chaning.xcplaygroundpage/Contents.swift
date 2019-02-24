535932ae3d1dffacb8c34f560de415e55db1dbc6
i4e <jun8d.mail@gmail.com>
2019-02-25 01:28:11 +0900

    feat: update optional chaining

--- SwiftLanguageGuide.playground/Pages/Optional Chaning.xcplaygroundpage/Contents.swift
+++ SwiftLanguageGuide.playground/Pages/Optional Chaning.xcplaygroundpage/Contents.swift
@@ -17,21 +17,174 @@ class Residence {
 
 let john = Person()
 
-//let roomCount = john.residence!.numberOfRooms
-// this triggers a runtime error
+// 強制アンラップはオプショナル値がnilのときエラー
+// residence が nil なので実行時エラー
+//let roomCount = john.residence!.numberOfRooms // error
 
-if let roomCount = john.residence?.numberOfRooms {
-    print("John's residence has \(roomCount) room(s).")
-} else {
-    print("Unable to retrieve the number of rooms.")
-}
-// Prints "Unable to retrieve the number of rooms."
+// residence が nil なので nil を返す
+john.residence?.numberOfRooms // nil
 
 john.residence = Residence()
 
-if let roomCount = john.residence?.numberOfRooms {
-    print("John's residence has \(roomCount) room(s).")
-} else {
-    print("Unable to retrieve the number of rooms.")
+john.residence?.numberOfRooms // 1
+
+// オプショナルチェーン呼び出しの結果は常にオプショナル
+// 値がある場合
+type(of: john.residence?.numberOfRooms)  // Optional<Int>
+// 値がnilの場合
+let paul = Person()
+type(of: paul.residence?.numberOfRooms)  // Optional<Int>
+
+// オプショナル型
+var a: String? = "hoge"
+//print(a+"a") // error
+a = nil
+// print(a+"a") // error
+
+// 有値オプショナル型
+var b: String! = "hoge"
+print(b+"a")
+b = nil
+//print(b+"a") // error
+
+
+//: ## Defining Model Classes for Optional Chaining
+
+class Person2 {
+    var residence: Residence2?
 }
-// Prints "John's residence has 1 room(s)."
+
+class Residence2 {
+    var rooms = [Room]()
+    var numberOfRooms: Int {
+        return rooms.count
+    }
+    subscript(i: Int) -> Room {
+        get {
+            return rooms[i]
+        }
+        set {
+            rooms[i] = newValue
+        }
+    }
+    func printNumberOfRooms() {
+        print(numberOfRooms)
+    }
+    var address: Address?
+}
+
+class Room {
+    let name: String
+    init(name: String) { self.name = name }
+}
+
+class Address {
+    var buildingName: String?
+    var buildingNumber: String?
+    var street: String?
+    func buildingIdentifier() -> String? {
+        if let buildingNumber = buildingNumber, let street = street {
+            return "\(buildingNumber) \(street)"
+        } else if buildingName != nil {
+            return buildingName
+        } else {
+            return nil
+        }
+    }
+}
+
+//: ## Accessing Properties Through Optional Chaining
+
+let ringo = Person2()
+ringo.residence?.numberOfRooms // nil
+
+let someAddress = Address()
+someAddress.buildingNumber = "29"
+someAddress.street = "Acacia Road"
+ringo.residence?.address = someAddress // 失敗
+ringo.residence?.address // nil なので，上のコードが失敗していることがわかる
+
+func createAddress() -> Address {
+    print("Function was called")
+    
+    let someAddress = Address()
+    someAddress.buildingNumber = "29"
+    someAddress.street = "Acacia Road"
+    
+    return someAddress
+    
+}
+ringo.residence?.address = createAddress() // 何も出力されていないため、関数 createAddress() が呼びだされていないとわかる
+
+
+//: ## Calling Methods Through Optional Chaining
+
+// オプショナル値のメソッドを呼び出すため、およびメソッド呼び出しが成功するかを確認するためにオプショナルチェーンを利用できる
+
+// printNumberOfRooms() メソッドを呼び出せるかチェック
+// Void を返すメソッドを optional chaining で呼び出した場合は Void? を返す
+if ringo.residence?.printNumberOfRooms() != nil {
+    print("success")
+} else {
+    print("failure")
+}
+
+// プロパティを設定できるかチェック
+// optional chaining によるプロパティの設定は Void? を返す
+if (ringo.residence?.address = someAddress) != nil {
+    print("success")
+} else {
+    print("failure")
+}
+
+
+//: ## Accessing Subscripts Through Optional Chaining
+
+ringo.residence?[0].name
+ringo.residence?[0] = Room(name: "Bathroom")    // 失敗
+
+let ringosHouse = Residence2()
+ringosHouse.rooms.append(Room(name: "Living Room"))
+ringosHouse.rooms.append(Room(name: "Kitchen"))
+ringo.residence = ringosHouse
+
+ringo.residence?[0].name    // Living Room
+
+
+//: ## Accessing Subscripts of Optional Type
+
+var testScores = ["Dave": [86, 82, 84], "Bev": [79, 94, 81]]
+testScores["Dave"]?[0] = 91
+testScores["Bev"]?[0] += 1
+testScores["Brian"]?[0] = 72    // 失敗
+// "Dave" 配列は [91, 82, 84] で、"Bev" 配列は [80, 94, 81]
+
+
+//: ## Linking Multiple Levels of Chaining
+
+// オプショナルチェーンで Int 値を取り出そうとした場合、使用されたチェーンのレベルにかかわらず、常に Int? が返されます。
+// 同様に、オプショナルチェーンで Int? 値を取り出そうとした場合、使用されたチェーンのレベルにかかわらず、常に Int? が返されます。
+
+// ringo.residence は有効
+// ringo.residence.address は nil
+ringo.residence?.address?.street    // nil
+
+let ringoAddress = Address()
+ringoAddress.buildingName = "The Larches"
+ringoAddress.street = "Laurel Street"
+ringo.residence?.address = ringoAddress
+
+ringo.residence?.address?.street    // "Laurel Street"
+
+
+//: ## Chaining on Methods with Optional Return Values
+
+
+ringo.residence?.address?.buildingIdentifier()
+type(of: ringo.residence?.address?.buildingIdentifier()) // String?
+
+ringo.residence?.address?.buildingIdentifier()?.hasPrefix("The")
+type(of: ringo.residence?.address?.buildingIdentifier()?.hasPrefix("The")) // Bool?
+// この例では、チェーンするオプショナル値が buildingIdentifier() メソッド自体ではなく、buildingIdentifier() メソッドの戻り値であるため、丸括弧の後にオプショナルチェーンのクエスチョンマークを置いています。
+
+
